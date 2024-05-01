c666e2b479fbf1f7da7e0bcfe8234f69b0511154
AlvaroS3rrano <a.serranor.2021@alumnos.urjc.es>
2024-05-01 12:35:53 +0200

    Changes to CustomErrorListener

--- src/CustomErrorListener.java
+++ src/CustomErrorListener.java
@@ -1,6 +1,4 @@
-import org.antlr.v4.runtime.BaseErrorListener;
-import org.antlr.v4.runtime.RecognitionException;
-import org.antlr.v4.runtime.Recognizer;
+import org.antlr.v4.runtime.*;
 
 public class CustomErrorListener extends BaseErrorListener {
 
@@ -9,18 +7,38 @@ public class CustomErrorListener extends BaseErrorListener {
         this.type = i;
     }
 
-    @Override
     public void syntaxError(Recognizer<?, ?> recognizer,
                             Object offendingSymbol,
                             int line, int charPositionInLine,
                             String msg,
-                            RecognitionException e) {
-        String message = "";
-        if (type == 1){
-            message = "Error de léxico en la línea ";
-        } else if (type == 2){
-            message = "Error de sintaxis en la línea ";
+                            RecognitionException e)
+    {
+        String RType = "";
+        if (type == 0){
+            RType = "lexico";
+        } else {
+            RType = "sintáctico";
         }
-        System.err.println(message + line + " y columna " + charPositionInLine + " - " + msg);
+        System.err.println("Error " + RType + " en la línea "+line+" y columna "+charPositionInLine+" -> "+msg);
+        underlineError(recognizer,(Token)offendingSymbol,
+                line, charPositionInLine);
+    }
+
+    protected void underlineError(Recognizer recognizer,
+                                  Token offendingToken, int line,
+                                  int charPositionInLine) {
+        CommonTokenStream tokens =
+                (CommonTokenStream)recognizer.getInputStream();
+        String input = tokens.getTokenSource().getInputStream().toString();
+        String[] lines = input.split("\n");
+        String errorLine = lines[line - 1];
+        System.err.println(errorLine);
+        for (int i=0; i<charPositionInLine; i++) System.err.print(" ");
+        int start = offendingToken.getStartIndex();
+        int stop = offendingToken.getStopIndex();
+        if ( start>=0 && stop>=0 ) {
+            for (int i=start; i<=stop; i++) System.err.print("^");
+        }
+        System.err.println();
     }
 }
